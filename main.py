392cd71ee265bba14034853404a38e0aba421dfb
Jiaqing Lin <Jiaqing930@gmail.com>
2017-05-26 11:52:30 +0900

    update

--- main.py
+++ main.py
@@ -15,8 +15,6 @@ def read_data(datatype='train'):
     Get training dataset if set datatype='train'.
     Get testing dataset if set datatype='test'.
     """
-    image_file = str()
-    label_file = str()
     # Set each file path.
     if datatype is 'train':
         image_file = "./dataset/train-images-idx3-ubyte"
@@ -26,12 +24,12 @@ def read_data(datatype='train'):
         label_file = "./dataset/t10k-labels-idx1-ubyte"
 
     if image_file and label_file:
-        # Read label data from given file.
+        # Read labels data from given file.
         with open(label_file, 'rb') as label_f:
             magic, num_item = struct.unpack('>II', label_f.read(8))
             label_tmp = np.fromfile(label_f, dtype=np.uint8)
             labels = label_tmp.reshape((np.int32(num_item), np.int32(1)))
-        # Read image data from given file.
+        # Read images data from given file.
         with open(image_file, 'rb') as image_f:
             magic, num_item, rows, cols = struct.unpack('>IIII', image_f.read(16))
             img_tmp = np.fromfile(image_f, dtype=np.uint8)
@@ -61,7 +59,9 @@ def training(images_train, labels_train):
     start_t = time.time()
     data_num, inChannel = images_train.shape
     error = []
+    # Number of neurons in hidden layer.
     hidden_num = 40
+    # Number of train target.
     outChannel_num = 10
     learning_rate = 0.2
     np.random.seed(1)
@@ -77,9 +77,9 @@ def training(images_train, labels_train):
         l0 = images_train[i]
         l1 = sigmoid(np.dot(l0, w1) + hidden_offset)
         l2 = sigmoid(np.dot(l1, w2) + outChannel_offset)
-        # Link each layers.
         l2_error = label - l2
         error.append(np.mean(np.abs(l2_error)))
+        # Link each layers.
         l2_delta = l2_error * sigmoid(l2, derive=True)
         l1_error = l2_delta.dot(w2.T)
         l1_delta = l1_error * sigmoid(l1, derive=True)
@@ -114,5 +114,7 @@ def testing(images_test, labels_test, w1, w2, hidden_offset, outChannel_offset):
 if __name__ == "__main__":
     images_train, labels_train = read_data(datatype='train')
     images_test, labels_test = read_data(datatype='test')
+    # Training.
     w1, w2, hidden_offset, outChannel_offset = training(images_train, labels_train)
+    # Testing.
     testing(images_test, labels_test, w1, w2, hidden_offset, outChannel_offset)
